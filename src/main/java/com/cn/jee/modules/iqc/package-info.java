38ed915137ccc215558f2c29a891fdd7b3677594
陈炽鹏 <1002360@chinaskin.cn>
2017-04-05 11:02:51 +0800

    条码

--- src/main/java/com/cn/jee/modules/iqc/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * 
- */
-/**
- * @author 1002360
- * @version 2017年3月30日下午1:51:55 1002360 TODO
- */
-package com.cn.jee.modules.iqc;
--- src/test/java/com/cn/jee/tools/SnGen.java
+++ src/test/java/com/cn/jee/tools/SnGen.java
@@ -8,13 +8,16 @@ public class SnGen {
 		long startT = System.currentTimeMillis();
 
 		// 通过UUID的哈希值产生16位SN
-		getSnByUUIdTest();
+		getSnByUUIdTest(1);
+
+		// 条码校验
+		barCodeVerificationTest("6911989251236");
 
 		long endT = System.currentTimeMillis();
 		System.out.println("耗时：" + (endT - startT));
 	}
 
-	/** 通过UUID的哈希值产生16位SN - 大于10000有可能重复 */
+	/** 通过UUID的哈希值产生16位SN - 大于20000有可能重复 */
 	public synchronized static String getSnByUUId() {
 		int machineId = 1;// 最大支持1-9个集群机器部署
 		int hashCodeV = UUID.randomUUID().toString().hashCode();
@@ -28,27 +31,58 @@ public class SnGen {
 		return machineId + String.format("%015d", hashCodeV);
 	}
 
+	/**
+	 * 条码校验
+	 * @param barCode
+	 * @return
+	 */
+	public final static boolean barCodeVerification(String barCode) {
+		if (barCode.matches("^\\d{13}$")) {
+			int c1 = Integer.parseInt(barCode.charAt(0) + "") + Integer.parseInt(barCode.charAt(2) + "") + Integer.parseInt(barCode.charAt(4) + "") + Integer.parseInt(barCode.charAt(6) + "") + Integer.parseInt(barCode.charAt(8) + "") + Integer.parseInt(barCode.charAt(10) + "");
+			int c2 = (Integer.parseInt(barCode.charAt(1) + "") + Integer.parseInt(barCode.charAt(3) + "") + Integer.parseInt(barCode.charAt(5) + "") + Integer.parseInt(barCode.charAt(7) + "") + Integer.parseInt(barCode.charAt(9) + "") + Integer.parseInt(barCode.charAt(11) + "")) * 3;
+			int c3 = Integer.parseInt(barCode.charAt(12) + "");
+			int cc = 10 - (c1 + c2) % 10;
+			if (cc == 10) {
+				cc = 0;
+			}
+			if (cc == c3) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	/*测试区*/
+	
 	/** 通过UUID的哈希值产生16位SN - 测试 */
-	private final static void getSnByUUIdTest() {
-		int s = 10000;
-		long[] ary = new long[s];
-		for (int i = 0; i < s; i++) {
+	private final static void getSnByUUIdTest(int loop) {
+		long[] ary = new long[loop];
+		for (int i = 0; i < loop; i++) {
 			ary[i] = Long.parseLong(getSnByUUId());
 		}
 
 		// 升序排序
 		// Arrays.sort(ary);
 
-		TAG_1: for (int i = 0; i < s; i++) {
-			for (int j = i + 1; j < s; j++) {
+		TAG_1: for (int i = 0; i < loop; i++) {
+			for (int j = i + 1; j < loop; j++) {
 				if (ary[i] == ary[j]) {
 					System.out.println(ary[i] + " - " + ary[j]);
 					break TAG_1;
 				}
 			}
-			if (i == s - 1) {
-				System.out.println("没有重复");
+			if (i == loop - 1) {
+				System.out.println("通过UUID的哈希值产生16位SN没有重复");
 			}
 		}
 	}
+	
+	/**
+	 * 条码校验 - 测试
+	 * @param barCode
+	 */
+	private final static void barCodeVerificationTest(String barCode){
+		System.out.println("条码校验结果:"+barCodeVerification(barCode));;
+	}
+
 }
